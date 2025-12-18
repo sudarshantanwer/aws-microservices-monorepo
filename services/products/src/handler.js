eadcc5419b084e360bea91fbd2ebdd609c24d65f
Sudarshan Tanwar <sudarshan.tanwar.ttn@sightly.com>
2025-12-19 01:49:42 +0530

    Refactor microservices to use relative imports for shared utilities and add API Gateway outputs for each service

--- services/products/src/handler.js
+++ services/products/src/handler.js
@@ -1,4 +1,4 @@
-import { ok, log } from "@shared/utils/src/response.js";
+import { ok, log } from "../shared/src/response.js";
 
 export const handler = async event => {
   log("Products service called", event);
